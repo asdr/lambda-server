f2682468a013dd8c9de357e0ae52b533279df3bf
Serdar Gokcen <home@sgokcen.com>
2018-11-11 02:18:45 +0300

    Made minor changes in defroute macro.

--- src/api.lisp
+++ src/api.lisp
@@ -12,21 +12,31 @@
    (cl-ppcre:all-matches-as-strings ":[A-Za-z0-9]+" string-url-rule)
    :test #'equal))
 
+(defun intern-symbol (name &key keyword)
+  (if keyword
+      (intern (string-upcase name) :keyword)
+      (intern (string-upcase name))))
+
 (defmacro append-web-context-root (url)
   (let ((normalized-url (string-right-trim "/" url)))
-    `(concatenate 'string "/" (symbol-value (intern (string-upcase "*web-context-root*"))) ,normalized-url)))
+    `(concatenate 'string "/" (symbol-value (intern-symbol "*web-context-root*")) ,normalized-url)))
 
 (defmacro defroute ((string-url-rule &rest args &key method identifier regexp &allow-other-keys) &body body)
-  (let ((params (gensym))
+  (let ((gparams (gensym))
         (parameter-names (get-parameter-names string-url-rule)))
     `(setf (ningle:route lambda-server:*server* (append-web-context-root ,string-url-rule) ,@args)
-           #'(lambda (,params)
-               (let ,(cons
-                      `(,(intern (string-upcase "unnamed-parameter-list")) (cdr (assoc :splat ,params)))
-                      (mapcar
-                       #'(lambda (p)
-                           `,(list
-                              (intern (string-upcase (subseq p 1) ))
-                              `(cdr (assoc ,(intern (string-upcase (subseq p 1)) :keyword) ,params))))
-                       parameter-names))
+           #'(lambda (,gparams)
+               (let* ,(cons
+                       `(,(intern-symbol "unnamed-parameter-list") (cdr (assoc :splat ,gparams)))
+                       (mapcar
+                        #'(lambda (p)
+                            `,(list
+                               (intern-symbol (subseq p 1))
+                               `(cdr (assoc ,(intern-symbol (subseq p 1) :keyword t) ,gparams))))
+                        parameter-names))
+                 ,(when parameter-names
+                    `(declare (ignorable ,@(mapcar
+                                            #'(lambda (p)
+                                                (intern-symbol (subseq p 1)))
+                                            parameter-names))))
                  ,@body)))))
